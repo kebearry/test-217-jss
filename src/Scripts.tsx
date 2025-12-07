6136fec8cba432d32f56f26f7ae2716b26086bce
Rebecca Tan <litl@sitecore.net>
2025-12-08 01:23:47 +0800

    initial commit

---
+++ src/Scripts.tsx
@@ -0,0 +1,17 @@
+import { JSX } from 'react';
+// The BYOC bundle imports external (BYOC) components into the app and makes sure they are ready to be used
+import BYOC from 'src/byoc';
+import CdpPageView from 'components/CdpPageView';
+import FEAASScripts from 'components/FEAASScripts';
+
+const Scripts = (): JSX.Element => {
+  return (
+    <>
+      <BYOC />
+      <CdpPageView />
+      <FEAASScripts />
+    </>
+  );
+};
+
+export default Scripts;
